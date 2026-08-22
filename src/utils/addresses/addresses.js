3f42c2675bc104b7d1e79896297b091dee231c72
Ryo <rrblx511@gmail.com>
2026-08-22 13:23:30 +0200

    Create addresses.js

---
+++ src/utils/addresses/addresses.js
@@ -0,0 +1,5 @@
+import { base } from "../utils.js";
+
+export const MessageManager = base.add(0x12346D8);
+export const SpectatePopupVTable = base.add(0x112CCC0);
+export const CustomButton_onButtonPressed = base.add(0xC4DFCC);
